d38df47e7676ef40c6cada4382fb5e369394603c
Jonathan Farrell <jonathan.farrell11@gmail.com>
2021-03-01 21:59:11 +0000

    flask basic pages and data base request calls

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+from jinja2 import Template
+from sqlalchemy import create_engine
+import config
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -6,6 +10,27 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+
+@app.route("/about")
+def about():
+    return app.send_static_file("about.html")
+
+
+@app.route("/bikes")
+def dynamic_bikes():
+    # Request Data from API
+    engine = create_engine(f"mysql+mysqlconnector://{config.user}:{config.passw}@{config.uri}:3306/wheelieGood",
+                           echo=True)
+    df = pd.read_sql("SELECT * from dynamic_bikes", engine)
+    weather_data = df.head(3).to_json(orient="records")
+    return weather_data
+
+
+@app.route("/contact")
+def contact():
+    d = {'name': 'Team Wheelie Good'}
+    return render_template("contact.html", **d)
 
 
 if __name__ == "__main__":
